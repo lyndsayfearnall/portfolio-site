9b3328104fdc1300f8d6eeb3d85ed1c3c456db17
Lyndsay Fearnall <lyndsayfearnall@Lyndsays-MacBook-Pro-2.local>
2018-01-08 15:28:46 -0500

    changed cdn files

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <link rel="stylesheet" href="node_modules/materialize-css/dist/css/materialize.css">
+  <!-- <link rel="stylesheet" href="node_modules/materialize-css/dist/css/materialize.css"> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="css/main.css">
 
@@ -44,6 +45,8 @@
     </div>
   </footer>
 
-<script src="node_modules/materialize-css/dist/js/materialize.js"></script>
+<!-- <script src="node_modules/materialize-css/dist/js/materialize.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
 </body>
 </html>
