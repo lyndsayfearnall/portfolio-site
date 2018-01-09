12ae8498b6d8cd4fe68b879e2c599957e67d9f65
Lyndsay Fearnall <lyndsayfearnall@Lyndsays-MacBook-Pro-2.local>
2018-01-09 16:53:08 -0500

    minor fomratting

--- includes/home.php
+++ includes/home.php
@@ -3,15 +3,13 @@
   <div class="container row">
 
     <img src="assets/logo.svg" alt="Lyndsay Fearnall's Logo" id="logo">
-    
+
     <div class="col s10 offset-s1 m8 offset-m2 l10 offset-l1">
       <p>I’m a front-end website developer and interactive media designer located in London, Ontario</p>
     </div>
     <div class="col s8 offset-s2 center-align" id="view-work">
       <button class="btn btn-large"><a href="#portfolio">View My Work</a></button>
     </div>
-
-
           <img src="assets/blue_keyboard.svg" alt="Laptop keyboard" id="keyboard">
 
           <img src="assets/blue_notebook.svg" alt="Notebook" id="notebook">
